478d6e32a91a49775e2d230f6aec1f8b606f0fc3
Simran Kaur <46941235+simranchanna@users.noreply.github.com>
2019-07-06 23:37:48 +0530

    Add files via upload

---
+++ src/one.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+import bbcNews from './bbc-news.js'
+
+export default class One extends Component{
+    render(){
+        return(
+            <div>
+                <bbcNews />
+            </div>
+        )
+    }
+}
